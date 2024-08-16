7c91f9947d0cbbacf4bf2c3b79a5214dfe65f3a6
Eshwar-138 <eshwardasari138@gmail.com>
2024-08-16 09:30:44 +0530

    FIRST PATTERN

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ for i in range(n):
     for j in range(i+1):
         print("*",end=" ")
     print()
-
+print()
 for i in range(n,0,-1):
     for j in range(i):
         time.sleep(0.6)
-        print(i,end=" ")
+        print("*",end=" ")
     print()
