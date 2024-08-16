e61dda5b2b66b053b6279b7de9e248b2d1e65571
Eshwar-138 <eshwardasari138@gmail.com>
2024-08-16 13:23:13 +0530

    lambda function tutorial

--- GITLEARNING.py
+++ GITLEARNING.py
@@ -1,3 +1,9 @@
-l=[1,2,3]
-l2=[3,4,5,6,7]
-print(list(set(l+l2)))
+# learning about lambda function
+#it is written in 1 line
+#think of it as a shortcut
+
+double=lambda a:a*2
+print(double(2))
+
+fullname=lambda first_name,last_name: "Welcome to Git "+first_name+" "+last_name
+print(fullname("Eshwar", "Dasari"))
