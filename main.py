2503b2ff241b6e1c6f4e69420bb9b653f7f53d9f
Eshwar-138 <eshwardasari138@gmail.com>
2024-08-16 09:29:33 +0530

    FIRST PATTERN

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import time
 n=5
 for i in range(n):
     for j in range(i+1):
-        time.sleep(0.78)
         print("*",end=" ")
+    print()
+
+for i in range(n,0,-1):
+    for j in range(i):
+        time.sleep(0.6)
+        print(i,end=" ")
     print()
